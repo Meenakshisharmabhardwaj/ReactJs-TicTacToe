e921be3598483a7f39759073fe0e6bbe90eb5b93
Meenakshi Sharma Bhardwaj <Meenakshisharmab651@gmail.com>
2020-10-31 15:17:27 +0530

    Game Completed

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {FaTimes,FaRegCircle,FaPen} from 'react-icons/fa';
-const Icon=({name})=>{
-   switch(name){
-   case 'circle':
-       return <FaRegCircle className='icons'></FaRegCircle>
-   case 'cross':
-       return <FaTimes className='icons'></FaTimes>
-   default:
-       return <FaRegCircle className='icons'></FaRegCircle>
-   }
-}
+import React from "react";
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa";
+
+const Icon = ({ name }) => {
+  switch (name) {
+    case "circle":
+      return <FaRegCircle className="icons" />;
+    case "cross":
+      return <FaTimes className="icons" />;
+    default:
+      return <FaPen className="icons" />;
+  }
+};
+
 export default Icon;
